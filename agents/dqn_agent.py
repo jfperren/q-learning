41bbe0d04a422c8b4b0b4dca35b9b82bb5c2cbff
Julien Perrenoud <julien.perrenoud@jfperren.io>
2020-05-03 02:28:56 +0000

    Updated from GC

--- agents/dqn_agent.py
+++ agents/dqn_agent.py
@@ -10,39 +10,6 @@ from agents.behavior.exploration_behavior import ExplorationBehavior
 
 class DQNAgent(ExplorationBehavior):
 
-    def __init__(self, env, strategy, model, optimizer, loss, discount):
-        super().__init__(env, strategy)
-        self.env = env
-        self.model = model
-        self.optimizer = optimizer
-        self.loss = loss
-        self.discount = discount
-
-    def best_action(self, state):
-        action = np.argmax(self.model.forward(torch.Tensor(state)).detach()).item()
-        return action
-
-    def learn(self, state, action, reward, next_state):
-
-        # Initialize optimizer for this step
-        self.optimizer.zero_grad()
-
-        # Target is the current Q-Value with new reward for action taken
-        out = self.model.forward(torch.Tensor(state))
-        future_reward = self.model.forward(torch.Tensor(next_state)).detach().max().item()
-        target = copy.deepcopy(out.detach())
-        target[action] = reward + self.discount * future_reward
-
-        # Move towards target with one backward pass
-        loss = self.loss(out, target.detach())
-        loss.backward()
-        for param in self.model.parameters():
-            param.grad.data.clamp_(-1, 1)
-        self.optimizer.step()
-
-
-class DQNAgent(ExplorationBehavior):
-
     def __init__(
         self, 
         strategy,
@@ -53,7 +20,9 @@ class DQNAgent(ExplorationBehavior):
         discount,
         memory_size=20000,
         batch_size=32,
-        skip=20
+        max_grad_norm=1,
+        skip=20,
+        target_update_frequency=10
     ):
 
         if skip == 0:
@@ -67,29 +36,56 @@ class DQNAgent(ExplorationBehavior):
         self.memory_size = memory_size
         self.batch_size = batch_size
         self.skip = skip
+        self.max_grad_norm = max_grad_norm
         self.memory = deque(maxlen=memory_size)
         self.epoch = 0
+        self.target_update_frequency = target_update_frequency
+        
+        if target_update_frequency is not None:
+            self.target_model = copy.deepcopy(self.model)
+        else:
+            self.target_model = None
 
     def should_skip(self, epoch):
-        return self.skip is not None and epoch % self.skip == 0
+        return self.skip is not None and epoch % self.skip != 0
+    
+    def has_target_network(self):
+        return self.target_update_frequency is not None
+    
+    def should_synchronize_networks(self, epoch):
+        return self.has_target_network() and epoch % self.target_update_frequency == 0
+    
+    def synchronyze_networks(self):
+        if self.target_model is not None:
+            self.target_model = copy.deepcopy(self.model)
 
-    def forward(self, state):
+    def forward(self, state, use_target=False):
         state = np.reshape(state, [1, self.env.observation_space.shape[0]])
-        return self.model.forward(torch.Tensor(state))
+        if use_target:
+            return self.target_model.forward(torch.Tensor(state))
+        else:
+            return self.model.forward(torch.Tensor(state))
 
-    def predict(self, state):
+    def predict(self, state, use_target=False):
         state = np.reshape(state, [1, self.env.observation_space.shape[0]])
         with torch.no_grad():
-            return self.model(torch.Tensor(state))
+            if use_target:
+                return self.target_model(torch.Tensor(state))
+            else:
+                return self.model(torch.Tensor(state))
 
     def best_action(self, state):
-        return self.predict(state).argmax().item()
-        
-    def random_action(self, state):
-        return np.random.randint(0, self.env.action_space.n)
+        # Best action always uses the current network, not the target one.
+        return self.predict(state, use_target=False).argmax().item()
     
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
+        
+    def on_episode_end(self, context):
+        super().on_episode_end(context)
+        
+        if self.should_synchronize_networks(context.get_episode_value('epoch')):
+            self.synchronyze_networks()
 
     def learn(self, state, action, reward, next_state, done, context):
         
@@ -108,25 +104,60 @@ class DQNAgent(ExplorationBehavior):
         for state, action, reward, state_next, done in batch:
 
             # Target is the current Q-Value with new reward for action taken
-            target = self.predict(state)
+            # If we have a target network, this is the one that should be used
+            # for estimating the future rewards.
+            target = self.predict(state, use_target=self.has_target_network())
             
             if not done:
-                future_reward = self.predict(next_state).max().item()
+                future_reward = self.predict(next_state, use_target=self.has_target_network()).max().item()
                 target[0][action] = reward + self.discount * future_reward
             else:
                 target[0][action] = reward
 
             # Move towards target with one backward pass
-            q_values = self.forward(state)
+            q_values = self.forward(state, use_target=False)
             loss = self.loss(q_values, target)
             
             self.optimizer.zero_grad()
             
-            # for param in self.model.parameters():
-            #     param.grad.data.clamp_(-1, 1)
-            
             loss.backward()
+            
+            # Gradient clipping
+            if self.max_grad_norm is not None:        
+                norm = torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1)   
+                context.append_episode_value('gradient_norm', norm)
+                
+#             if loss.detach() > 500:
+#                 print('BEFORE STEP')
+#                 print('epoch: {}'.format(context.get_episode_value('epoch')))
+#                 print('loss: {}'.format(loss.detach()))
+#                 print('state: {}'.format(state))
+#                 print('action: {}'.format(action))
+#                 print('next state: {}'.format(next_state))
+#                 print('done: {}'.format(done))
+#                 print('q_values: {}'.format(q_values))
+#                 print('target: {}'.format(target))
+
             self.optimizer.step()
+            
+#             if loss.detach() > 500:
+#                 target = self.predict(state)
+#                 if not done:
+#                     future_reward = self.predict(next_state, False).max().item()
+#                     target[0][action] = reward + self.discount * future_reward
+#                 else:
+#                     target[0][action] = reward
+#                 q_values = self.forward(state)
+#                 loss = self.loss(q_values, target)
+#                 print('AFTER STEP')
+#                 print('epoch: {}'.format(context.get_episode_value('epoch')))
+#                 print('loss: {}'.format(loss.detach()))
+#                 print('state: {}'.format(state))
+#                 print('action: {}'.format(action))
+#                 print('next state: {}'.format(next_state))
+#                 print('done: {}'.format(done))
+#                 print('q_values: {}'.format(q_values))
+#                 print('target: {}'.format(target))
             
             context.append_episode_value('loss', loss.detach())
             context.append_episode_value('target', target[0][action].detach())
--- observers/__init__.py
+++ observers/__init__.py
@@ -1,4 +1,7 @@
 from observers.state_analysis_logger import StateAnalysisLogger
 from observers.window_metric_logger import WindowMetricLogger
 from observers.window_step_metric_logger import WindowStepMetricLogger
-from observers.tensorboard_scalar_logger import TensorboardScalarLogger
+from observers.tensorboard_logger import (
+    TensorboardScalarLogger,
+    TensorboardHistogramLogger
+)
